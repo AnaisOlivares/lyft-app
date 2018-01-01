f71a8e5dfbdb7a2edc3a62af6b814c236762d5e2
AnaisOlivares <anaisolivareshilario@gmail.com>
2017-12-31 22:34:04 -0500

    añadiendo vista datos

--- js/verify.js
+++ js/verify.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
   var number = Math.floor(Math.random() * (400 - 100) + 100);
-
+  // volver a enviar código
   $('#resend').on('click', function() {
     alert('Tu código: ' + 'LAB-' + number);
+  });
+  // siguiente
+  $('#verify-code').on('keyup', function() {
+    if ($('#verify-code').val().length === 3) {
+      $('#next-verif').removeClass('disabled');
+    } else {
+      $('#next-verif').addClass('disabled');
+    }
   });
 });
